e9c6eee0fe139317f948be4a243927df95bf5da2
navpuks <127082336+navpuks@users.noreply.github.com>
2023-03-14 00:24:56 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; ++t) {
+        int h, w, n;
+        cin >> h >> w >> n;
+        int x = n % h;
+        int y = n / h + 1;
+        if (x == 0) {
+            x = h;
+            --y;
+        }
+        cout << x;
+        cout.width(2);
+        cout.fill('0');
+        cout << y << '\n';
+    }
+
+    return 0;
+}
