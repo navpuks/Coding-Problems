b803c6a1ae3e173e236a71b17453da81caa7495d
navpuks <127082336+navpuks@users.noreply.github.com>
2023-03-14 00:25:35 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    while (true) {
+        string s;
+        getline(cin, s);
+        if (cin.eof()) break;
+
+        cout << s << '\n';
+    }
+
+    return 0;
+}
