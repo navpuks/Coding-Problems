a750a950effc925e03a94f178fbcd4faa2f5d332
navpuks <127082336+navpuks@users.noreply.github.com>
2023-03-14 00:33:37 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    vector<int> x(3), y(3);
+    for (int i = 0; i < 3; ++i) {
+        cin >> x[i] >> y[i];
+    }
+    
+    if (x[0] == x[1]) cout << x[2];
+    else if (x[0] == x[2]) cout << x[1];
+    else cout << x[0];
+    cout << ' ';
+
+    if (y[0] == y[1]) cout << y[2];
+    else if (y[0] == y[2]) cout << y[1];
+    else cout << y[0];
+    
+    return 0;
+}
